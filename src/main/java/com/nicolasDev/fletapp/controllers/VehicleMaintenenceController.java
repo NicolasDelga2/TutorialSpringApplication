9c1ac7162741f9701eb7a20a46401b2f423323c2
NicolasDel <delgadonicolas251@gmail.com>
2021-01-25 20:54:35 -0300

    25/01/2021 Terminando Controllers

--- src/main/java/com/nicolasDev/fletapp/controllers/VehicleMaintenenceController.java
+++ src/main/java/com/nicolasDev/fletapp/controllers/VehicleMaintenenceController.java
@@ -1,13 +1,57 @@
 package com.nicolasDev.fletapp.controllers;
 
+import com.nicolasDev.fletapp.models.VehicleMaintenance;
+import com.nicolasDev.fletapp.services.SupplierService;
+import com.nicolasDev.fletapp.services.VehicleMaintenenceService;
+import com.nicolasDev.fletapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class VehicleMaintenenceController {
 
+    @Autowired
+    private VehicleMaintenenceService vehicleMaintenenceService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @Autowired
+    private SupplierService supplierService;
+
     @GetMapping("/vehicleMaintenence")
-    public String getVehicleMaintenence(){
+    public String getVehicleMaintenence(Model model){
+        model.addAttribute("vehicles" , vehicleService.getVehicles());
+        model.addAttribute("suppliers", supplierService.getSuppliers());
+        model.addAttribute("vehiclesMaintenances", vehicleMaintenenceService.getVehicleMaintenance());
         return "vehicleMaintenence";
+    }
+
+    @PostMapping("/vehicleMaintenence/addNew")
+    public String addNew(VehicleMaintenance vehicleMaintenance){
+        vehicleMaintenenceService.addNew(vehicleMaintenance);
+        return "redirect:/vehicleMaintenence";
+    }
+
+    @RequestMapping("/vehicleMaintenence/findById")
+    @ResponseBody
+    public Optional<VehicleMaintenance> findById(Integer id){
+        return vehicleMaintenenceService.findById(id);
+    }
+
+    @RequestMapping(value = "/vehicleMaintenence/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(VehicleMaintenance vehicleMaintenence){
+        vehicleMaintenenceService.update(vehicleMaintenence);
+        return "redirect:/vehicleMaintenence";
+    }
+
+    @RequestMapping(value = "/vehicleMaintenence/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id){
+        vehicleMaintenenceService.delete(id);
+        return "redirect:/vehicleMaintenence";
     }
 }
--- src/main/java/com/nicolasDev/fletapp/services/VehicleMaintenenceService.java
+++ src/main/java/com/nicolasDev/fletapp/services/VehicleMaintenenceService.java
@@ -1,7 +1,35 @@
 package com.nicolasDev.fletapp.services;
 
+import com.nicolasDev.fletapp.models.VehicleMaintenance;
+import com.nicolasDev.fletapp.repositories.VehicleMaintenenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMaintenenceService {
+    @Autowired
+    private VehicleMaintenenceRepository vehicleMaintenenceRepository;
+
+    public List<VehicleMaintenance> getVehicleMaintenance(){
+        return vehicleMaintenenceRepository.findAll();
+    }
+
+    public void addNew(VehicleMaintenance vehicleMaintenance){
+        vehicleMaintenenceRepository.save(vehicleMaintenance);
+    }
+
+    public void update(VehicleMaintenance vehicleMaintenence){
+        vehicleMaintenenceRepository.save(vehicleMaintenence);
+    }
+
+    public void delete(Integer id){
+        vehicleMaintenenceRepository.deleteById(id);
+    }
+
+    public Optional<VehicleMaintenance> findById(Integer id){
+        return vehicleMaintenenceRepository.findById(id);
+    }
 }
